7c366320d3963c886fb0365adbdc72e1926538b4
Caroline-Patrick <carolinespatrick@gmail.com>
2022-07-29 13:13:42 -0500

    changes to local file organization

---
+++ intro-js/scripts.js
@@ -0,0 +1,8 @@
+
+
+const disappear = (element) => {
+    console.log ("element was clicked. And the ID is: " + element.id)
+    // console.log(element.style)
+
+    document.getElementById(element.id).style.display="none"
+}
